b2b59d24eccb41e3e65669e18191ef6708b68330
syhan7516 <syhan7516@naver.com>
2022-09-23 23:59:01 +0900

    파이어베이스 셋팅

--- react-firebase/src/components/App.js
+++ react-firebase/src/components/App.js
@@ -1,11 +1,20 @@
-import AppRouter from "./Router";
-
+import { useState } from 'react';
+import AppRouter from './Router';
+import { firebaseInstance } from '../firebaseInfo';
+import { dbService } from '../firebaseInfo';
 
 function App() {
+
+  // firebase value test code
+  console.log(firebaseInstance);
+
+  const [isLoggedIn,setIsLoggedIn] = useState(false);
+
   return (
-    <div>
-      <AppRouter/>
-    </div>
+    <>
+      <AppRouter isLoggedIn={isLoggedIn}/>
+      <footer>&copy; {new Date().getFullYear()} Firebase Test </footer>
+    </>
   );
 }
 
--- react-firebase/src/index.js
+++ react-firebase/src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 
-// firebase file import 후 확인
-import firebase from './firebase';
-console.log(firebase);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

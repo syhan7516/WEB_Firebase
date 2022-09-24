0da38f4128152761aacc81c237fc9eff6b82f240
syhan7516 <syhan7516@naver.com>
2022-09-24 23:59:27 +0900

    파이어베이스 기본 셋팅

--- react-firebase/src/firebaseInfo.js
+++ react-firebase/src/firebaseInfo.js
@@ -1,20 +1,18 @@
-// firebase file 생성 및 코드 추가
-import * as firebase from "firebase/app";
-import 'firebase/firestore';
+// 필요한 SDK에서 필요한 기능 가져오기
+import { initializeApp } from "firebase/app";
 
-// Your web app's Firebase configuration
+// 사용할 Firebase 제품에 대한 SDK를 추가합니다.
+// 공식문서 : https://firebase.google.com/docs/web/setup#available-libraries
+
+// 웹 앱의 Firebase 객체 구성 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    // databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
-    appId: process.env.REACT_APP_APP_ID,
-  };
+  apiKey: process.env.REACT_APP_APP_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
 
-firebase.initializeApp(firebaseConfig);
-
-export const firebaseInstance = firebase;
-
-export const dbService = firebase.firestore();
+// 파이어베이스 초기화
+export const app = initializeApp(firebaseConfig);

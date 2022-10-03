30cd7d52dfcdb5c70024cd169683e4d20fabb0ad
syhan7516 <syhan7516@naver.com>
2022-10-03 14:37:22 +0900

    Home 컴포넌트 채팅 폼 프로토타입

--- react-firebase/src/routes/Home.js
+++ react-firebase/src/routes/Home.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import { React, useState } from 'react'
+// Firebase 앱, 디비 객체 불러오기
+import { dbService } from 'firebaseInfo';
+// 디비에 데이터 추가 ( 폴더, 문서 )
+import { collection, addDoc } from 'firebase/firestore';
 
 const Home = () => {
+
+    const [chat,setChat] = useState('');
+
+    // 채팅 전송
+    const submitContent = async (event) => {
+
+        event.preventDefault();
+
+        await addDoc(collection(dbService, 'chats'),
+        {
+            text : chat,
+            createdAt : Date.now(),
+        });
+
+        setChat('');
+    }
+
+    const writeContent = (event) => { 
+        setChat(event.target.value) 
+    }
+
     return (
-        <div>Home</div>
+        <div>
+            <h2>Home</h2>
+            <form>
+                <input type="text" placeholder="입력" value={chat} onChange={writeContent}/>
+                <button onClick={submitContent}>전송</button>
+            </form>
+        </div>
     )
 }
 
